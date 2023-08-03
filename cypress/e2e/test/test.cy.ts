530c970377c9fce086d1319cccaea247f47c3f32
Krzysztof Bogusz <bogusz.web@gmail.com>
2023-08-03 12:10:09 +0200

    Add cypress

---
+++ cypress/e2e/test/test.cy.ts
@@ -0,0 +1,11 @@
+describe('Test Suite', () => {
+  it('2 + 2 = 4', () => {
+    expect(2 + 2).to.equal(4);
+  });
+
+  it('Visits localhost:4200 and checks title', () => {
+    cy.visit('http://localhost:4200');
+
+    cy.get('[data-cy="title"]').should('contain', 'cypress-cicd');
+  });
+});
