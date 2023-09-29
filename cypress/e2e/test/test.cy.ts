c4728345c078c713e5f5d7a2ec5670f6df2f59eb
Krzysztof Bogusz <bogusz.web@gmail.com>
2023-09-29 15:19:58 +0200

    WEB-19997 Testing electron run

--- cypress/e2e/test/test.cy.ts
+++ cypress/e2e/test/test.cy.ts
@@ -2,6 +2,10 @@ describe('Test Suite', () => {
   it('2 + 2 = 4', () => {
     expect(2 + 2).to.equal(4);
   });
+  it('2 + 2 = 4', () => {
+    expect(2 + 5).to.equal(4);
+  });
+
 
   it('Visits localhost:4200 and checks title', () => {
     cy.visit('http://localhost:4200');
